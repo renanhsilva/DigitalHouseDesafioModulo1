f3eded89764e9232fafc99c7557d62a7f24f3346
Renan Silva <renanh2008@hotmail.com>
2020-09-25 22:20:14 -0300

    Criando a Classe Professor. Parte C

--- src/br/digitalhouse/desafio1/Curso.kt
+++ src/br/digitalhouse/desafio1/Curso.kt
@@ -1,5 +1,5 @@
 package br.digitalhouse.desafio1
 
 class Curso(val nome: String, val codCurso: Int) {
-    
+
 }
---
+++ src/br/digitalhouse/desafio1/Professor.kt
@@ -0,0 +1,4 @@
+package br.digitalhouse.desafio1
+
+class Professor(val nome: String, val sobrenome: String, val tempoDeCasa: Int, val codProfessor: Int) {
+}
