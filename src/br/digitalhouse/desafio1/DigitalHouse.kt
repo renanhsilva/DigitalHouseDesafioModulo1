9b80886dac1cd4430568d14e6d7b5e4e591f19f4
Renan Silva <renanh2008@hotmail.com>
2020-09-29 23:49:54 -0300

    Parte G

---
+++ src/br/digitalhouse/desafio1/DigitalHouse.kt
@@ -0,0 +1,5 @@
+package br.digitalhouse.desafio1
+
+class DigitalHouse(var listaDeAlunos: MutableList<Aluno>, var listaDeProfessores: MutableList<Professor>, var listaDeCursos: MutableList<Curso>, var listaDeMatriculas: MutableList<Matricula>) {
+
+}
