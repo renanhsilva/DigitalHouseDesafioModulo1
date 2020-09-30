53dd7453bb750bbbd516fcccf267a3c8f6a53287
Renan Silva <renanh2008@hotmail.com>
2020-09-29 23:29:07 -0300

    Parte E

--- src/br/digitalhouse/desafio1/Curso.kt
+++ src/br/digitalhouse/desafio1/Curso.kt
@@ -1,5 +1,5 @@
 package br.digitalhouse.desafio1
 
-class Curso(val nome: String, val codCurso: Int) {
+class Curso(val nome: String, val codCurso: Int, val professorTitular: ProfessorTitular, val professorAdjunto: ProfessorAdjunto, val qtdmaxAlunos: Int, val aluno: Aluno, val listaAluno: MutableList<Aluno>) {
 
 }
