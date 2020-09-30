6ffe17b044c45481425d4cdb1ec943b21d0cf79b
Renan Silva <renanh2008@hotmail.com>
2020-09-30 00:26:34 -0300

    Parte H

--- src/br/digitalhouse/desafio1/main.kt
+++ src/br/digitalhouse/desafio1/main.kt
@@ -2,4 +2,15 @@ package br.digitalhouse.desafio1
 
 fun main(){
 
+    val aluno1 = Aluno("Renan","Silva",1)
+    val aluno2 = Aluno("Gustavo","Santos",2)
+    val aluno3 = Aluno("Liz","Siso",3)
+
+    val professor1 = Professor("Carlos","Adão",1,1)
+    val professor2 = Professor("Vitoria","Gonçalves",5,2)
+
+    val professorTitular1 = ProfessorTitular("Mestre em Kotlin")
+
+    //val curso1 = Curso("Desenvolvimento em Kotlin",2,)
+
 }
